9022c091342f65b71f44b9ec51ac426fbaf4be21
Brandon3118 <brandon.afonso3118@gmail.com>
2026-08-03 19:14:23 +0200

    add firsst lines to show a black screen

--- main.c
+++ main.c
@@ -9,5 +9,14 @@
 #define FPS 165
 
 int main(int argc, char* argv[]) {
+
+    SDL_Window* window = SDL_CreateWindow("Roguelike",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
