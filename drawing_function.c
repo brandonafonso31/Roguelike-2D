e069ffb084da3cd17550969075ee4dfa0d5ad32f
Brandon3118 <brandon.afonso3118@gmail.com>
2026-08-04 16:45:47 +0200

    use my old code to add fps display

--- drawing_function.c
+++ drawing_function.c
@@ -1,1 +1,12 @@
-//need to write multiple function to draw from this file
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+void drawText(SDL_Renderer* renderer, TTF_Font* font, const char* text, int x, int y) {
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text, color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect dstRect = {x, y, surface->w, surface->h};
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+}
--- main.c
+++ main.c
@@ -3,17 +3,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "drawing_function.c"
+
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 #define FPS 30
+#define FPS_HISTORY 30
 
+//--------------------- Gestion FPS ---------------------//
+typedef struct {
+    double history[FPS_HISTORY];
+    int index;
+} FPSCounter;
+
+void FPSCounter_Init(FPSCounter* counter) {
+    for (int i = 0; i < FPS_HISTORY; i++) counter->history[i] = 1.0 / FPS; // init à 165 FPS
+    counter->index = 0;
+}
+
+int FPSCounter_Update(FPSCounter* counter, double deltaTime) {
+    counter->history[counter->index] = deltaTime;
+    counter->index = (counter->index + 1) % FPS_HISTORY;
+
+    double sum = 0;
+    for (int i = 0; i < FPS_HISTORY; i++) sum += counter->history[i];
+    double averageDelta = sum / FPS_HISTORY;
+
+    return (int)(0.5 + 1.0 / averageDelta);
+}
+
+//---------------------- Dessin -----------------------//
 void main_menu(SDL_Window* window, SDL_Renderer* renderer){
 
-    int running = 1, current_time = 0;
+    int running = 1;
     Uint64 lastCounter = SDL_GetPerformanceCounter(), currentCounter;
     double delta_time;
     SDL_Event event;
+
+    FPSCounter fpsCounter;
+    FPSCounter_Init(&fpsCounter);
+    char fpsText[50];
+
+    TTF_Font* font = TTF_OpenFont("pokemon_BW2.otf", 24);
+    if (!font) {
+        printf("Erreur chargement police: %s\n", TTF_GetError());
+        TTF_Quit();
+        return;
+    }
 
     while (running){
 
@@ -22,12 +59,17 @@ void main_menu(SDL_Window* window, SDL_Renderer* renderer){
         lastCounter = currentCounter;
 
         while (SDL_PollEvent(&event)){
-            if (event.type == SDL_QUIT)
-                running = 0;
+            if (event.type == SDL_QUIT) running = 0;
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+
+
+        // Afficher FPS
+        int fpsAverage = FPSCounter_Update(&fpsCounter, delta_time);
+        snprintf(fpsText, sizeof(fpsText), "FPS: %d", fpsAverage);
+        drawText(renderer, font, fpsText, 10, 10);
 
         SDL_RenderPresent(renderer);
     }
