5137c5b45ffb9c59553f1449e17298ad23cb69e5
Brandon3118 <brandon.afonso3118@gmail.com>
2026-08-04 17:41:12 +0200

    adjsut to read correctly settings

--- settings.c
+++ settings.c
@@ -1,13 +1,34 @@
 #include "settings.h"
 #include <stdio.h>
-#include "settings.cfg"
+#include <string.h>
+ 
+void loadSettings(GameSettings* settings)
+{
+    FILE* file = fopen("settings.cfg", "r");
+    if (!file){
+        printf("Impossible d'ouvrir settings.cfg\n");
+        return;
+    }
 
-void loadSettings(GameSettings* settings){    
-    settings->width = WINDOW_WIDTH;
-    settings->height = WINDOW_HEIGHT;
-    settings->fps_limit = FPS_;
-    settings->vsync = VSYNC;
-    settings->volume = VOLUME;
+    char key[50];
+    int value;
 
+    while(fscanf(file, "%[^=]=%d\n", key, &value) == 2){
+        if(strcmp(key,"WINDOW_WIDTH")==0)
+            settings->width = value;
+
+        else if(strcmp(key,"WINDOW_HEIGHT")==0)
+            settings->height = value;
+
+        else if(strcmp(key,"FPS")==0)
+            settings->fps_limit = value;
+
+        else if(strcmp(key,"VSYNC")==0)
+            settings->vsync = value;
+
+        else if(strcmp(key,"VOLUME")==0)
+            settings->volume = value;
+    }
+    fclose(file);
     printf("Settings loaded\n");
 }
