7915ffebed007af9807b25995e70a7beee050328
Brandon3118 <brandon.afonso3118@gmail.com>
2026-08-16 16:58:07 +0200

    remove useless comment and add some more relevant

--- src/poll_events.c
+++ src/poll_events.c
@@ -25,6 +25,7 @@ int pollEventsMenu(MainMenu* menu, SDL_Event* event, GameState* state){
                             break;
                         case MENU_SETTINGS:
                             printf("Ouverture des settings\n");
+                            // openSettingsEdit()
                             break;
                         case MENU_QUIT:
                             printf("Fermeture du jeu !\n");
@@ -59,43 +60,17 @@ int pollEventsInGameMenu(InGameMenu* menu, SDL_Event* event, GameState* state) {
         if (event->type == SDL_KEYDOWN) {
             switch(event->key.keysym.sym) {
                 case SDLK_RETURN:
-                    /*InGameMenuAction action = getSelectedActionInGameMenu(menu);
-                    switch(action) {
-                        case SHOP:
-                            //enter sub-menu shop
-                            break;
-                        case INVENTORY:
-                            //enter sub-menu inventory
-                            break;
-                        case WORLD:
-                            //enter sub-menu world
-                            break;
-                        case UPGRADES:
-                            //enter sub-menu upgrades
-                            break;
-                        case WIP:
-                            //enter sub-menu wip
-                            break;
-                        case INGAME_PLAY:
-                            *state = STATE_GAME_PLAY;
-                            break;
-                        case INGAME_BACK:
-                            //get out of a sub menu
-                            break;
-                        case INGAME_QUIT:
-                            running = 0;
-                            *state = STATE_QUIT;
-                            break;
-                    }*/
                     break;                    
                 case SDLK_ESCAPE:
                     running = 0;
                     *state = STATE_MAIN_MENU;
                     break;
                 case SDLK_LEFT:
+                    printf("%d", menu->selected_tab);
                     menu->selected_tab = (menu->selected_tab - 1 + menu->button_count) % menu->button_count;
                     break;
                 case SDLK_RIGHT:
+                    printf("%d", menu->selected_tab);
                     menu->selected_tab = (menu->selected_tab + 1) % menu->button_count;
                     break;
             }
