33a82c8e7604224046614cac0067b1fad5b44d71
Brandon3118 <brandon.afonso3118@gmail.com>
2026-09-08 21:03:51 +0200

    add missing includes + name for entity

--- src/entity.c
+++ src/entity.c
@@ -1,11 +1,19 @@
 #include "entity.h"
 
-Entity* initEntity(Statistics* stats){
-    Entity* entity;
-    if (!stats){
-        stats = setBasicStats();
+Entity* initEntity(const char* name, Statistics* stats){
+    Entity* entity = (Entity*)malloc(sizeof(Entity));
+
+    if (!name){
+        strcpy(entity->name, "Unknown");
+    } else {
+        strncpy(entity->name, name, sizeof(entity->name) - 1);
+        entity->name[sizeof(entity->name) - 1] = '\0';
     }
 
-    entity->stats = stats;
+    if (!stats){
+        stats = setBasicStats();
+    } else entity->stats = stats;
+
+
     return entity;
 }
--- src/entity.h
+++ src/entity.h
@@ -1,12 +1,17 @@
 #ifndef Entity_H
 #define Entity_H
 
+#include <stdlib.h>
+#include <string.h>
 #include "statistics.h"
 
+#define MAX_NAME_LENGTH 32
+
 typedef struct {
+    char name[MAX_NAME_LENGTH];
     Statistics* stats;
 } Entity;
 
-Entity* initEntity(Statistics* stats);
+Entity* initEntity(const char* name, Statistics* stats);
 
 #endif
