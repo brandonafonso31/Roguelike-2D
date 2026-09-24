be81f89577fc26c2e925e1fba77a09f6954ff547
Brandon3118 <brandon.afonso3118@gmail.com>
2026-09-24 17:24:29 +0200

    init gameplay loop

--- include/gameplay.h
+++ include/gameplay.h
@@ -1,4 +1,17 @@
 #ifndef GAMEPLAY_H
 #define GAMEPLAY_H
 
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
+#include "settings.h"
+#include "fps_utils.h"
+#include "paths.h"
+#include "poll_events.h"
+#include "ui.h"
+#include "menu.h"
+#include "game_state.h"
+#include "json_loader.h"
+
+GameState gameplay(SDL_Window* window, SDL_Renderer* renderer, GameSettings* settings, EntityDatabase* db_entities);
+
 #endif
--- src/gameplay.c
+++ src/gameplay.c
@@ -1,2 +1,40 @@
 #include "gameplay.h"
 
+GameState gameplay(SDL_Window* window, SDL_Renderer* renderer, GameSettings* settings, EntityDatabase* db_entities){
+
+    SDL_Event event;
+    char font_full_path[2048];
+    snprintf(font_full_path, sizeof(font_full_path), "%s/%s", getFontsPath(), settings->font);
+    TTF_Font* font = TTF_OpenFont(font_full_path, 24);
+    if (!font) {
+        printf("Erreur chargement police : %s\n", TTF_GetError());
+        return MENU_QUIT;
+    }
+    
+    InGameMenu* menu = inGameMenuCreate(renderer, settings);
+    if (!menu) {
+        printf("Erreur création du menu\n");
+        return MENU_QUIT;
+    }
+
+    FPSCounter fps_counter;
+    fpsCounterInit(&fps_counter, settings->fps_limit);
+    double dt;
+    
+    int running = 1;
+    GameState state = STATE_GAME_PLAY;
+    while (running) {
+        //---------------- INPUT ----------------//
+        //running = pollEventsGameplay(menu, &event, &state); 
+        //---------------- Delta ----------------//
+        dt = fpsCounterGetDeltaTime(&fps_counter);
+        //---------------- RENDER ---------------//
+        //renderInterfaceInGame(menu, &fps_counter, renderer, dt, font, settings, db_entities);
+        //---------------- FPS LIMIT ----------------//
+        waitOrNot(&fps_counter, settings);
+    }
+
+    TTF_CloseFont(font);
+    destroyInGameMenu(menu);
+    return state;
+}
