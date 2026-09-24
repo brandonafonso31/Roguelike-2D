89afdea8117df517740cf915c79c708da0ddfd42
Brandon3118 <brandon.afonso3118@gmail.com>
2026-09-24 18:50:25 +0200

    add poll event for gameplay (basic)

--- include/poll_events.h
+++ include/poll_events.h
@@ -4,10 +4,11 @@
 #include <SDL2/SDL.h>
 #include "menu.h"
 #include "game_state.h"
+#include "ingame_interface.h"
 
 int pollEventsMenu(MainMenu* menu,SDL_Event* event, GameState* state);
 int pollEventsInGameMenu(InGameMenu* menu, SDL_Event* event, GameState* state);
 int pollEventsSettingsMenu(SettingsMenu* menu, SDL_Event* event, GameState* state);
-//int pollEventsGameplay(SettingsMenu* menu, SDL_Event* event, GameState* state);
+int pollEventsGameplay(Interface* interface, SDL_Event* event, GameState* state);
 
 #endif
