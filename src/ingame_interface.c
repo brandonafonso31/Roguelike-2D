00058e9b87e7f333e43be71e9872a71f5977f8d7
Brandon3118 <brandon.afonso3118@gmail.com>
2026-09-24 18:51:18 +0200

    add init for new attrtibut

--- src/ingame_interface.c
+++ src/ingame_interface.c
@@ -8,6 +8,12 @@ Interface* interfaceInit(SDL_Renderer* renderer, Entity* player) {
     
     interface->player = player;
     interface->background = loadImageMenu(renderer, "world1.png");
+
+    interface->move_up = 0;
+    interface->move_down = 0;
+    interface->move_left = 0;
+    interface->move_right = 0;
+    
     return interface;
 }
 
